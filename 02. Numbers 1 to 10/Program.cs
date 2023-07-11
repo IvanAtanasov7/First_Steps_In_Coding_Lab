ce9bf46f8e2c91568201380ffa28337bcb8d325d
IvanAtanasov7 <ivanatanasov630@gmail.com>
2023-07-11 11:13:29 +0300

    Add project files.

---
+++ 02. Numbers 1 to 10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02._Numbers_1_to_10
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ 08. Pet Shop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _08._Pet_Shop
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int foodForDogs = int.Parse(Console.ReadLine());
+			int foodForCats = int.Parse(Console.ReadLine());
+						
+			double totalSum = foodForDogs * 2.50 + foodForCats * 4;
+			Console.WriteLine($"{totalSum} lv.");
+		}
+	}
+}
